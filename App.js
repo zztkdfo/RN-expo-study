a3d0dd72c3e81bee904cf4f188c811a4c8569f5a
zztkdfo_m1 <zztkdfo@gmail.com>
2024-08-14 18:21:11 +0900

    Feat: React native expo study

---
+++ App.js
@@ -0,0 +1,77 @@
+import { useState } from "react"
+
+import { StatusBar } from "expo-status-bar"
+import { StyleSheet, Text, View, Image } from "react-native"
+
+import ImageViewer from "./components/ImageViewer"
+import Button from "./components/Button"
+import * as ImagePicker from "expo-image-picker"
+
+const PlaceholderImage = require("./assets/images/background-image.png")
+
+export default function App() {
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    })
+
+    if (!result.canceled) {
+      console.log(result)
+      setSelectedImage(result.assets[0].uri)
+    } else {
+      alert("You did not select any image.")
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+        <ImageViewer
+          placeholderImageSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
+      </View>
+
+      <View style={styles.footerContainer}>
+        <Button
+          theme="primary"
+          label="Choose a photo"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this photo" />
+      </View>
+
+      <StatusBar style="auto" />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+
+    // borderColor: "#fff",
+    // borderWidth: 1,
+  },
+
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+    // borderColor: "#fff",
+    // borderWidth: 1,
+  },
+
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
+
+    // borderColor: "#fff",
+    // borderWidth: 1,
+  },
+})
